666d1fd4eea37fdbabeba6f90af929633d3fe79e
Andrés David Álvarez Tórrez <ndralvz@gmail.com>
2023-12-28 20:54:59 -0300

    :wrench::lipstick: formateamos el ejercicio 1

--- ejercicioUno.js
+++ ejercicioUno.js
@@ -1,29 +1,32 @@
 "use strict"
 
 const alumnos = [
-    {
-        nombre: "Julieta",
-        edad: 27,
-    },
-    {
-        nombre: "Lucas",
-        edad: 23,
-    },
-    {
-        nombre: "Andrés",
-        edad: 33,
-    },
-    {
-        nombre: "Maria",
-        edad: 18,
-    },
+  {
+    nombre: "Julieta",
+    edad: 27,
+  },
+  {
+    nombre: "Lucas",
+    edad: 23,
+  },
+  {
+    nombre: "Andrés",
+    edad: 33,
+  },
+  {
+    nombre: "Maria",
+    edad: 18,
+  },
 ]
 
 console.log("La cantidad de alumnos es: " + alumnos.length)
 
-let promedio = 0;
+let promedio = 0
+
 for (let index = 0; index < alumnos.length; index++) {
-    promedio = promedio + alumnos[index].edad
+  promedio = promedio + alumnos[index].edad
 }
 
-console.log("El promedio de edad de los alumnos es de: " + (promedio / alumnos.length))
+console.log(
+  "El promedio de edad de los alumnos es de: " + promedio / alumnos.length
+)
